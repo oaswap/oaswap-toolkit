27c2b0be76c5674cc4b570051ad532d8d35bab53
oaswap <oaswap@protonmail.com>
2022-01-12 08:36:51 -0600

    refactor: Fix footer links

--- packages/oaswap-uikit/src/components/Footer/config.tsx
+++ packages/oaswap-uikit/src/components/Footer/config.tsx
@@ -7,28 +7,28 @@ export const footerLinks: FooterLinkType[] = [
     items: [
       {
         label: "Contact",
-        href: "https://docs.pancakeswap.finance/contact-us",
+        href: "https://docs.oaswap.xyz/contact-us",
       },
       {
         label: "Blog",
-        href: "https://pancakeswap.medium.com/",
+        href: "https://oaswap.medium.com/",
       },
       {
         label: "Community",
-        href: "https://docs.pancakeswap.finance/contact-us/telegram",
+        href: "https://docs.oaswap.xyz/contact-us/telegram",
       },
       {
-        label: "CAKE",
-        href: "https://docs.pancakeswap.finance/tokenomics/cake",
+        label: "OAS",
+        href: "https://docs.oaswap.xyz/tokenomics/cake",
       },
-      {
-        label: "—",
-      },
-      {
-        label: "Online Store",
-        href: "https://pancakeswap.creator-spring.com/",
-        isHighlighted: true,
-      },
+      // {
+      //   label: "—",
+      // },
+      // {
+      //   label: "Online Store",
+      //   href: "https://oaswap.creator-spring.com/",
+      //   isHighlighted: true,
+      // },
     ],
   },
   {
@@ -36,15 +36,15 @@ export const footerLinks: FooterLinkType[] = [
     items: [
       {
         label: "Customer",
-        href: "Support https://docs.pancakeswap.finance/contact-us/customer-support",
+        href: "Support https://docs.oaswap.xyz/contact-us/customer-support",
       },
       {
         label: "Troubleshooting",
-        href: "https://docs.pancakeswap.finance/help/troubleshooting",
+        href: "https://docs.oaswap.xyz/help/troubleshooting",
       },
       {
         label: "Guides",
-        href: "https://docs.pancakeswap.finance/get-started",
+        href: "https://docs.oaswap.xyz/get-started",
       },
     ],
   },
@@ -53,24 +53,24 @@ export const footerLinks: FooterLinkType[] = [
     items: [
       {
         label: "Github",
-        href: "https://github.com/pancakeswap",
+        href: "https://github.com/oaswap",
       },
       {
         label: "Documentation",
-        href: "https://docs.pancakeswap.finance",
+        href: "https://docs.oaswap.xyz",
       },
-      {
-        label: "Bug Bounty",
-        href: "https://app.gitbook.com/@pancakeswap-1/s/pancakeswap/code/bug-bounty",
-      },
+      // {
+      //   label: "Bug Bounty",
+      //   href: "https://app.gitbook.com/@oaswap-1/s/oaswap/code/bug-bounty",
+      // },
       {
         label: "Audits",
-        href: "https://docs.pancakeswap.finance/help/faq#is-pancakeswap-safe-has-pancakeswap-been-audited",
+        href: "https://docs.oaswap.xyz/help/faq#is-oaswap-safe-has-oaswap-been-audited",
       },
-      {
-        label: "Careers",
-        href: "https://docs.pancakeswap.finance/hiring/become-a-chef",
-      },
+      // {
+      //   label: "Careers",
+      //   href: "https://docs.oaswap.xyz/hiring/become-a-chef",
+      // },
     ],
   },
 ];
@@ -79,93 +79,94 @@ export const socials = [
   {
     label: "Twitter",
     icon: "Twitter",
-    href: "https://twitter.com/pancakeswap",
+    href: "https://twitter.com/oaswap",
   },
   {
     label: "Telegram",
     icon: "Telegram",
-    items: [
-      {
-        label: "English",
-        href: "https://t.me/pancakeswap",
-      },
-      {
-        label: "Bahasa Indonesia",
-        href: "https://t.me/PancakeSwapIndonesia",
-      },
-      {
-        label: "中文",
-        href: "https://t.me/PancakeSwap_CN",
-      },
-      {
-        label: "Tiếng Việt",
-        href: "https://t.me/PancakeSwapVN",
-      },
-      {
-        label: "Italiano",
-        href: "https://t.me/pancakeswap_ita",
-      },
-      {
-        label: "русский",
-        href: "https://t.me/pancakeswap_ru",
-      },
-      {
-        label: "Türkiye",
-        href: "https://t.me/pancakeswapturkiye",
-      },
-      {
-        label: "Português",
-        href: "https://t.me/PancakeSwapPortuguese",
-      },
-      {
-        label: "Español",
-        href: "https://t.me/PancakeswapEs",
-      },
-      {
-        label: "日本語",
-        href: "https://t.me/pancakeswapjp",
-      },
-      {
-        label: "Français",
-        href: "https://t.me/pancakeswapfr",
-      },
-      {
-        label: "Datch",
-        href: "https://t.me/PancakeSwap_DE",
-      },
-      {
-        label: "Filipino",
-        href: "https://t.me/Pancakeswap_Ph",
-      },
-      {
-        label: "ქართული ენა",
-        href: "https://t.me/PancakeSwapGeorgia",
-      },
-      {
-        label: "Announcements",
-        href: "https://t.me/PancakeSwapAnn",
-      },
-    ],
+    href: "https://t.me/oaswap",
+    // items: [
+    //   {
+    //     label: "English",
+    //     href: "https://t.me/oaswap",
+    //   },
+    //   {
+    //     label: "Bahasa Indonesia",
+    //     href: "https://t.me/oaswapIndonesia",
+    //   },
+    //   {
+    //     label: "中文",
+    //     href: "https://t.me/oaswap_CN",
+    //   },
+    //   {
+    //     label: "Tiếng Việt",
+    //     href: "https://t.me/oaswapVN",
+    //   },
+    //   {
+    //     label: "Italiano",
+    //     href: "https://t.me/oaswap_ita",
+    //   },
+    //   {
+    //     label: "русский",
+    //     href: "https://t.me/oaswap_ru",
+    //   },
+    //   {
+    //     label: "Türkiye",
+    //     href: "https://t.me/oaswapturkiye",
+    //   },
+    //   {
+    //     label: "Português",
+    //     href: "https://t.me/oaswapPortuguese",
+    //   },
+    //   {
+    //     label: "Español",
+    //     href: "https://t.me/oaswapEs",
+    //   },
+    //   {
+    //     label: "日本語",
+    //     href: "https://t.me/oaswapjp",
+    //   },
+    //   {
+    //     label: "Français",
+    //     href: "https://t.me/oaswapfr",
+    //   },
+    //   {
+    //     label: "Datch",
+    //     href: "https://t.me/oaswap_DE",
+    //   },
+    //   {
+    //     label: "Filipino",
+    //     href: "https://t.me/oaswap_Ph",
+    //   },
+    //   {
+    //     label: "ქართული ენა",
+    //     href: "https://t.me/oaswapGeorgia",
+    //   },
+    //   {
+    //     label: "Announcements",
+    //     href: "https://t.me/oaswapAnn",
+    //   },
+    // ],
   },
-  {
-    label: "Reddit",
-    icon: "Reddit",
-    href: "https://reddit.com/r/pancakeswap",
-  },
-  {
-    label: "Instagram",
-    icon: "Instagram",
-    href: "https://instagram.com/pancakeswap_official",
-  },
+  // {
+  //   label: "Reddit",
+  //   icon: "Reddit",
+  //   href: "https://reddit.com/r/oaswap",
+  // },
+  // {
+  //   label: "Instagram",
+  //   icon: "Instagram",
+  //   href: "https://instagram.com/oaswap_official",
+  // },
   {
     label: "Github",
     icon: "Github",
-    href: "https://github.com/pancakeswap/",
+    href: "https://github.com/oaswap/",
   },
   {
     label: "Discord",
     icon: "Discord",
-    href: "https://discord.gg/pancakeswap",
+    href: "https://discord.gg/oaswap",
   },
 ];
 
