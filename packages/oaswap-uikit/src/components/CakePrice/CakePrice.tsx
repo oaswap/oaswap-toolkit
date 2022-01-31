3bcc8c0d654e7b09631deb84319c2beb349c5da4
oaswap <oaswap@protonmail.com>
2022-01-31 13:02:43 -0600

    refactor: Change token address in swap link

--- packages/oaswap-uikit/src/components/CakePrice/CakePrice.tsx
+++ packages/oaswap-uikit/src/components/CakePrice/CakePrice.tsx
@@ -26,7 +26,7 @@ const PriceLink = styled.a`
 const CakePrice: React.FC<Props> = ({ cakePriceUsd, color = "textSubtle" }) => {
   return cakePriceUsd ? (
     <PriceLink
-      href="https://oaswap.finance/swap?outputCurrency=0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82"
+      href="https://oaswap.finance/swap?outputCurrency=0x6D906f979ab61d43921212895F25FEcBCd2750E7"
       target="_blank"
     >
       <PancakeRoundIcon width="24px" mr="8px" />
